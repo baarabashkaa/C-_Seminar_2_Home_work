876de955eec3fb64415bb0eab7be427eedcabb9b
IlyaDubinin <ilya_k-somon96@mai.ru>
2023-07-23 20:33:40 +0800

    выполнил 3 задачу

--- Program.cs
+++ Program.cs
@@ -17,24 +17,74 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.WriteLine("ведите число");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("ведите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-if (num1>99)
-{
-int num2 = (num1 / 100 * 100);
-int num3 = (num1 % 10); 
-int numtotal = ((num1 - num2 - num3)/10);
-Console.WriteLine(numtotal);
-}
+// if (num1>99)
+// {
+// int num2 = (num1 / 100 * 100);
+// int num3 = (num1 % 10); 
+// int numtotal = ((num1 - num2 - num3)/10);
+// Console.WriteLine(numtotal);
+// }
 
-else if (num1>999)
-{
-int numtotal = (Index2);
-Console.WriteLine(numtotal);
-}
+// else if (num1>999)
+// {
+// int numtotal = (Index2);
+// Console.WriteLine(numtotal);
+// }
 
-else
-{
-   Console.WriteLine("третьей цифры нет"); 
-}
+// else
+// {
+//    Console.WriteLine("третьей цифры нет"); 
+// }
+
+
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("ведите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 == 1)
+// {
+//     Console.WriteLine("Понедельник - Нет");
+// }
+
+// else if (num1 == 2)
+// {
+//     Console.WriteLine("Вторник - Нет");
+// }
+
+// else if(num1 == 3)
+// {
+//     Console.WriteLine("Среда - Нет");
+// }
+
+// else if (num1 == 4)
+// {
+//     Console.WriteLine("Четверг - Нет");
+// }
+
+// else if(num1 == 5)
+// {
+//     Console.WriteLine("Пятница - Нет");
+// }
+
+// else if(num1 == 6)
+// {
+//     Console.WriteLine("Суббота - Да");
+// }
+
+// else if(num1 == 7)
+// {
+//     Console.WriteLine("Воскресенье - Да");
+// }
+
+// else
+// {
+//     Console.WriteLine("Нет такого дня недели");
+// }
