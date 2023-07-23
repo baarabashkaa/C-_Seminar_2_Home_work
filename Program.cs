faad69224954079bc06690180ee2f3aaaab638b9
IlyaDubinin <ilya_k-somon96@mai.ru>
2023-07-23 18:26:29 +0800

    выполнил первую задачу

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("ведите трёхзначное число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2 = (num1 / 100 * 100);
+int num3 = (num1 % 10); 
+int numtotal = ((num1 - num2 - num3)/10);
+
+Console.WriteLine(numtotal);
+
+
+
+
+
